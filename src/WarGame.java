62e69755580a7ef0d3d6d472cd44f249a4a19860
chondosha1234 <jmiller61193@gmail.com>
2023-09-12 16:38:16 -0400

    started wargame

--- src/WarGame.java
+++ src/WarGame.java
@@ -1,3 +1,33 @@
 public class WarGame {
+
+    private SQueue<Card> deck;
+    private SQueue<Card> player1Hand;
+    private SQueue<Card> player2Hand;
+    private SQueue<Card> player1Discard;
+    private SQueue<Card> player2Discard;
+
+    public WarGame() {
+        deck = new SQueue<>(52);
+        for (Card.Suits suit : Card.Suits.values()) {
+            for (Card.Ranks rank : Card.Ranks.values()) {
+                deck.enqueue(new Card(suit, rank));
+            }
+        }
+        deck.shuffle();
+
+        // each player hand and discard can have at most 52 cards
+        player1Hand = new SQueue<>(52);
+        player1Discard = new SQueue<>(52);
+        player2Hand = new SQueue<>(52);
+        player2Discard = new SQueue<>(52);
+
+        Card nextCard;
+        while (!deck.isEmpty()) {
+            nextCard = deck.dequeue();
+            player1Hand.enqueue(nextCard);
+            nextCard = deck.dequeue();
+            player2Hand.enqueue(nextCard);
+        }
+    }
 	
 }
